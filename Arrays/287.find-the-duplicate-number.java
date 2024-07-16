75b723453d84d94365a051ab40483e65a7826d3d
Chinmay Patil <chinmay9601@gmail.com>
2024-07-16 16:31:45 +0530

    Add files via upload

---
+++ Arrays/287.find-the-duplicate-number.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=287 lang=java
+ *
+ * [287] Find the Duplicate Number
+ */
+
+// @lc code=start
+
+import java.util.Arrays;
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<=nums.length;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
