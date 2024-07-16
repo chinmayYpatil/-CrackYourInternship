a56f801bc5142a726411586dcf6932365b2fe242
Chinmay Patil <75988613+chinmay7016@users.noreply.github.com>
2024-07-17 02:41:40 +0530

    day1

--- Arrays/26.remove-duplicates-from-sorted-array.java
+++ Arrays/26.remove-duplicates-from-sorted-array.java
@@ -7,7 +7,15 @@
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
+        int n=nums.length;
+        int newarr=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[newarr]=nums[i];
+                newarr++;
+            }
+        }
+        return newarr;
     }
 }
 // @lc code=end
