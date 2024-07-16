5b01af24eb4d0c7dcc5db350aa1541cc1175eeec
Chinmay Patil <chinmay9601@gmail.com>
2024-07-17 02:36:38 +0530

    Update 283.move-zeroes.java

--- Arrays/283.move-zeroes.java
+++ Arrays/283.move-zeroes.java
@@ -7,21 +7,21 @@
 // @lc code=start
 class Solution {
     public void moveZeroes(int[] nums) {
-        int newarr=0;
-        int n=nums.length;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==0){
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]!=0){
-                        int temp=nums[j];
-                        nums[j]=nums[i];
-                        nums[i]=temp;
-                        break;
-                        }
-                        }
-                        }
-                        }
-}
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < n; j++) {
+                    if (nums[j] != 0) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        break; 
+                    }
+                }
+            }
+        }
+    }
 }
 
 // @lc code=end
